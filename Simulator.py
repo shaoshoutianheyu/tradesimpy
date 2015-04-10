Fixed max drawdown computation, added max drawdown for benchmark analysis

Simulator.py
@@ -174,16 +174,24 @@ class Simulator(object):
             prev_invested_amount = invested_amount[date]
             prev_portfolio_value = portfolio_value[date]
 
-            # Monitor portfolio drawdown
-            if portfolio_value[date] > global_high:
-                global_high = portfolio_value[date]
-                local_low = global_high
-            elif portfolio_value[date] < local_low:
-                local_low = portfolio_value[date]
+            # Monitor portfolio drawdown (conservatively)
+            if len(purchased_shares) != 0:
+                # Price entire portfolio's day high and low
+                portfolio_high = cash_amount[date]
+                portfolio_low = cash_amount[date]
+                for key, value in purchased_shares.iteritems():
+                    portfolio_high += value*self.data[key].loc[date, 'High']
+                    portfolio_low += value*self.data[key].loc[date, 'Low']
 
-                # Record max drawdown
-                if ((local_low / global_high) - 1) < max_drawdown:
-                    max_drawdown = (local_low / global_high) - 1
+                if portfolio_high > global_high:
+                    global_high = portfolio_high
+                    local_low = global_high
+                elif portfolio_low < local_low:
+                    local_low = portfolio_low
+
+                    # Record max drawdown
+                    if ((local_low / global_high) - 1) < max_drawdown:
+                        max_drawdown = (local_low / global_high) - 1
 
         # Create data frame out of daily trade stats
         daily_results = pd.DataFrame(portfolio_value.values(), columns=['Portfolio Value'], index=portfolio_value.keys())
@@ -201,15 +209,16 @@ class Simulator(object):
         annual_semi_std_dev = daily_results['Return'].where(daily_results['Return'] < 0.0).std() * np.sqrt(252)
         years_traded = (((self.dates[-1] - self.dates[0]).days + 1) / 365.0)
         total_return = daily_results['Portfolio Value'][-1] / daily_results['Portfolio Value'][0]
+        cagr = total_return ** (1 / years_traded) - 1
 
         # Create dictionary out of period stats
-        # TODO: Attach params from trading_algo
         period_results = {
-            'Max Drawdown': max_drawdown,
+            'Max Drawdown': -max_drawdown,
             'Sharpe Ratio': annual_avg_return / annual_std_dev,
             'Sortino Ratio': annual_avg_return / annual_semi_std_dev,
+            'MAR Ratio': -cagr / max_drawdown,
             # 'Information Ratio': 0.0,
-            'CAGR': (total_return) ** (1 / years_traded) - 1,
+            'CAGR': cagr,
             'Total Return': total_return - 1,
             'Annual Return': annual_avg_return,
             'Annual Volatility': annual_std_dev,

optimizers/Optimizer.py
@@ -79,25 +79,33 @@ if __name__ == '__main__':
 
     # Pull benchmark data, scale, and get statistics for comparison analysis
     benchmark_ticker = 'SPY'
-    benchmark = di.load_data([benchmark_ticker], start_date, end_date, adjusted=True)[benchmark_ticker]['Close']
-    benchmark *= (capital_base / benchmark[0])
-    benchmark_returns = (benchmark - benchmark.shift(1)) / benchmark.shift(1)
+    benchmark_data = di.load_data([benchmark_ticker], start_date, end_date, adjusted=True)[benchmark_ticker]
+    benchmark_close = benchmark_data['Close'] * (capital_base / benchmark_data['Close'][0])
+    benchmark_returns = (benchmark_close - benchmark_close.shift(1)) / benchmark_close.shift(1)
     benchmark_avg_returns = benchmark_returns.mean() * 252
     benchmark_std_dev = benchmark_returns.std() * np.sqrt(252)
     benchmark_semi_std_dev = benchmark_returns.where(benchmark_returns < 0.0).std() * np.sqrt(252)
     benchmark_sharpe = benchmark_avg_returns / benchmark_std_dev
     benchmark_sortino = benchmark_avg_returns / benchmark_semi_std_dev
+    benchmark_cagr = (benchmark_close[-1] / benchmark_close[0]) ** (1 / ((end_date - start_date).days / 365.0)) - 1
+    global_high = 0.0
+    local_low = 0.0
+    benchmark_max_drawdown = 0.0
+    for p in benchmark_data.index:
+        if benchmark_data.ix[p]['High'] > global_high:
+            global_high = benchmark_data.ix[p]['High']
+            local_low = global_high
 
-    # print benchmark
-    # print benchmark_returns
-    # print benchmark_sharpe
-    # print benchmark_sortino
-    # exit(1)
+        if benchmark_data.ix[p]['Low'] < local_low:
+            local_low = benchmark_data.ix[p]['Low']
+
+            # Record max drawdown
+            if ((local_low / global_high) - 1) < benchmark_max_drawdown:
+                benchmark_max_drawdown = (local_low / global_high) - 1
 
     # Display a plot of the top N scenarios' portfolio value from the sorted results
     num_scenarios = 10
-    # port_value_series = pd.concat(list(benchmark), axis=0)
-    port_value_series = pd.DataFrame(benchmark)
+    port_value_series = pd.DataFrame(benchmark_close)
     for i in range(0, num_scenarios):
         port_value_series[str(results.head(num_scenarios).iloc[i]['Params'])] =\
             pd.Series(results.head(num_scenarios).iloc[i]['Portfolio Value'],
@@ -116,14 +124,22 @@ if __name__ == '__main__':
     results.to_csv(filename, index=False)
 
     print 'Benchmark results:'
-    print 'Total Return: %f' % ((benchmark[-1] / benchmark[0]) - 1)
+    print 'Total Return: %f' % ((benchmark_close[-1] / benchmark_close[0]) - 1)
+    print 'CAGR: %f' % (benchmark_cagr)
+    print 'Max Drawdown: %f' % (-benchmark_max_drawdown)
     print 'Sharpe Ratio: %f' % (benchmark_sharpe)
     print 'Sortino Ratio: %f' % (benchmark_sortino)
-    print 'Max Drawdown: %f' % (0.0)
-    print 'CAGR: %f\n' % ((benchmark[-1] / benchmark[0]) ** (1 / ((end_date - start_date).days / 365.0)) - 1)
+    print 'MAR Ratio: %f\n' % (-benchmark_cagr / benchmark_max_drawdown)
 
     print 'Top %d scenario results:' % (num_scenarios)
     print results.head(num_scenarios)[
-        ['Params', 'Sharpe Ratio', 'Sortino Ratio', 'Total Return', 'Max Drawdown', 'CAGR', 'Total Trades']
+        ['Params',
+         'Total Return',
+         'CAGR',
+         'Max Drawdown',
+         'Sharpe Ratio',
+         'Sortino Ratio',
+         'MAR Ratio',
+         'Total Trades']
     ]
     plt.show()