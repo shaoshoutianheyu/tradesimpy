0a7d5e282307e63b275ebec4f727b83168229efc
grantathon <grant.bartel@gmail.com>
2016-06-03 07:06:10 +0200

    Added more error handling for loading market data

--- tradesimpy/data/market_data.py
+++ tradesimpy/data/market_data.py
@@ -1,13 +1,15 @@
 import re
 import pandas as pd
 import quandl
-from pandas.tseries.offsets import BDay
+import numpy as np
 import data_source_factory as dsf
+from pandas.tseries.offsets import BDay
+from datetime import datetime
 
 def load_market_data(tickers, ticker_types, data_sources, start_date, end_date, history_window, csv_data_uri=None):
     # Check for unique tickers
     if(len(tickers) != len(list(set(tickers)))):
-        raise AttributeError("There are non-unique tickers attempted to be loaded.")
+        raise AttributeError("There are non-unique tickers set for loading.")
 
     # Create the unique data sources
     unique_data_sources = dsf.create_data_sources(data_sources, csv_data_uri)
@@ -22,13 +24,28 @@ def load_market_data(tickers, ticker_types, data_sources, start_date, end_date, 
         data_source = unique_data_sources[key]
         group_data = data_source.request(group['tickers'], group['ticker_types'], start_date, end_date, history_window)
 
-        # Check if there are equal tickers but with different data sources
-        for key in group_data.keys():
-            if(key in data.keys()):
-                raise KeyError("There is more than one of the same ticker name: %s" % key)
+        # Check if data request failed for all tickers
+        if(not group_data):
+            raise AttributeError("The data request failed for all tickers from data source %s" % key)
+
+        # Check if data request failed for certain tickers
+        for ticker in group['tickers']:
+            if(ticker not in group_data.keys()):
+                raise AttributeError("The data request failed for ticker %s from data source %s" % (ticker, key))
+
+        # Check for issues in data
+        for ticker_name, series in group_data.iteritems():
+            # Check for equal tickers but with different data sources
+            if(ticker_name in data.keys()):
+                raise AttributeError("There is more than one of the same ticker name loaded: %s" % ticker_name)
+
+            # Check for proper date ranges in data
+            # TODO: Should be set as warnings?
+            if(series.index[history_window-1] > start_date):
+                raise AttributeError("The data for ticker %s has a start date greater than %s: %s" % (key, start_date, series.index[0]))
+            if(series.index[-1] < end_date):
+                raise AttributeError("The data for ticker %s has a end date less than %s: %s" % (key, end_date, series.index[-1]))
 
         data.update(group_data)
-
-    # exit(0)
 
     return data
